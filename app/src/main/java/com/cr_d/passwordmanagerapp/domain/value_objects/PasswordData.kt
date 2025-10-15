8a3ca3fd090b350189ecd0a4915bf78c38934eda
Carlos Ramos <carlosramosdev@protonmail.com>
2025-10-15 13:08:24 +0200

    Added PasswordData as a value Object

---
+++ app/src/main/java/com/cr_d/passwordmanagerapp/domain/value_objects/PasswordData.kt
@@ -0,0 +1,16 @@
+package com.cr_d.passwordmanagerapp.domain.value_objects
+
+data class PasswordData (
+    val id : Int,
+    val account : String,
+    val password : String,
+    val hasLowerCase : Boolean,
+    val hasUpperCase : Boolean,
+    val hasNumbers : Boolean,
+    val hasSpecials : Boolean,
+    val application : String,
+    val url : String,
+    val creationDate : String,
+    val lastUpdate : String,
+    val securityScore : Double
+)
