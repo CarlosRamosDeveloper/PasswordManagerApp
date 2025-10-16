ac9cd10f3a389e2feacb5e63031b3e8b6c8f410b
Carlos Ramos <carlosramosdev@protonmail.com>
2025-10-16 10:38:09 +0200

    Implemented PasswordDataGeneration DTO in PasswordGenerator

--- app/src/main/java/com/cr_d/passwordmanagerapp/domain/entities/PasswordGenerator.kt
+++ app/src/main/java/com/cr_d/passwordmanagerapp/domain/entities/PasswordGenerator.kt
@@ -1,35 +1,33 @@
 package com.cr_d.passwordmanagerapp.domain.entities
 
+import com.cr_d.passwordmanagerapp.domain.value_objects.PasswordDataGeneration
+
 class PasswordGenerator (
-    val hasLowerCase: Boolean = true,
-    val hasUpperCase: Boolean = false,
-    val hasNumbers: Boolean = false,
-    val hasSpecials: Boolean = false,
-    var passwordLength: Int= PasswordPolicy.MIN_GENERATED_LENGTH,
+    var passwordDataGeneration: PasswordDataGeneration
     ){
 
     fun generatePassword(): String {
 
-        if (passwordLength < PasswordPolicy.MIN_GENERATED_LENGTH) passwordLength = PasswordPolicy.MIN_GENERATED_LENGTH
-        if (passwordLength > PasswordPolicy.MAX_LENGTH) passwordLength = PasswordPolicy.MAX_LENGTH
+        if (passwordDataGeneration.passwordLength < PasswordPolicy.MIN_GENERATED_LENGTH) passwordDataGeneration.passwordLength = PasswordPolicy.MIN_GENERATED_LENGTH
+        if (passwordDataGeneration.passwordLength > PasswordPolicy.MAX_LENGTH) passwordDataGeneration.passwordLength = PasswordPolicy.MAX_LENGTH
 
         val availableSets = buildList {
-            if (hasLowerCase) add(PasswordPolicy.LOWER_CHARS)
-            if (hasUpperCase) add(PasswordPolicy.UPPER_CHARS)
-            if (hasNumbers) add(PasswordPolicy.NUMBER_CHARS)
-            if (hasSpecials) add(PasswordPolicy.SYMBOL_CHARS)
+            if (passwordDataGeneration.hasLowerCase) add(PasswordPolicy.LOWER_CHARS)
+            if (passwordDataGeneration.hasUpperCase) add(PasswordPolicy.UPPER_CHARS)
+            if (passwordDataGeneration.hasNumbers) add(PasswordPolicy.NUMBER_CHARS)
+            if (passwordDataGeneration.hasSpecials) add(PasswordPolicy.SYMBOL_CHARS)
         }
 
         require(availableSets.isNotEmpty()) { "At least one character set must be selected" }
 
         val passwordChars = mutableListOf<Char>()
 
-        if (hasLowerCase) passwordChars += PasswordPolicy.LOWER_CHARS.random()
-        if (hasUpperCase) passwordChars += PasswordPolicy.UPPER_CHARS.random()
-        if (hasNumbers) passwordChars += PasswordPolicy.NUMBER_CHARS.random()
-        if (hasSpecials) passwordChars += PasswordPolicy.SYMBOL_CHARS.random()
+        if (passwordDataGeneration.hasLowerCase) passwordChars += PasswordPolicy.LOWER_CHARS.random()
+        if (passwordDataGeneration.hasUpperCase) passwordChars += PasswordPolicy.UPPER_CHARS.random()
+        if (passwordDataGeneration.hasNumbers) passwordChars += PasswordPolicy.NUMBER_CHARS.random()
+        if (passwordDataGeneration.hasSpecials) passwordChars += PasswordPolicy.SYMBOL_CHARS.random()
 
-        while (passwordChars.size < passwordLength) {
+        while (passwordChars.size < passwordDataGeneration.passwordLength) {
             val set = availableSets.random()
             passwordChars += set.random()
         }
--- app/src/test/java/com/cr_d/passwordmanagerapp/domain/entities/PasswordGeneratorTest.kt
+++ app/src/test/java/com/cr_d/passwordmanagerapp/domain/entities/PasswordGeneratorTest.kt
@@ -1,84 +1,125 @@
 package com.cr_d.passwordmanagerapp.domain.entities
 
+import com.cr_d.passwordmanagerapp.domain.value_objects.PasswordDataGeneration
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class PasswordGeneratorTest {
 
     @Test
-    fun `should create a default password`(){
-        val testSubject = PasswordGenerator()
-
-        Assertions.assertTrue(testSubject.hasLowerCase)
-        Assertions.assertFalse(testSubject.hasUpperCase)
-        Assertions.assertFalse(testSubject.hasNumbers)
-        Assertions.assertFalse(testSubject.hasSpecials)
-        Assertions.assertEquals(16, testSubject.passwordLength)
-    }
-
-    @Test
     fun should_generate_password_with_correct_length_when_length_within_limits() {
-        val generator = PasswordGenerator(
+        val data = PasswordDataGeneration(
             hasLowerCase = true,
             hasUpperCase = true,
             hasNumbers = true,
             hasSpecials = true,
             passwordLength = 20
         )
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
+
         Assertions.assertEquals(20, password.length)
     }
 
     @Test
     fun should_generate_password_with_min_generated_length_when_length_too_short() {
-        val generator = PasswordGenerator(passwordLength = 5)
+        val data = PasswordDataGeneration(
+            hasLowerCase = true,
+            hasUpperCase = true,
+            hasNumbers = true,
+            hasSpecials = true,
+            passwordLength = 5
+        )
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
+
         Assertions.assertTrue(password.length >= PasswordPolicy.MIN_GENERATED_LENGTH)
     }
 
     @Test
     fun should_generate_password_with_max_length_when_length_too_long() {
-        val generator = PasswordGenerator(passwordLength = 200)
+        val data = PasswordDataGeneration(
+            hasLowerCase = true,
+            hasUpperCase = true,
+            hasNumbers = true,
+            hasSpecials = true,
+            passwordLength = 200
+        )
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
+
         Assertions.assertTrue(password.length <= PasswordPolicy.MAX_LENGTH)
     }
 
     @Test
     fun should_include_lowercase_characters_when_enabled() {
-        val generator = PasswordGenerator(hasLowerCase = true)
+        val data = PasswordDataGeneration(
+            hasLowerCase = true,
+            hasUpperCase = false,
+            hasNumbers = false,
+            hasSpecials = false,
+            passwordLength = 20
+        )
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
+
         Assertions.assertTrue(password.any { it.isLowerCase() })
     }
 
     @Test
     fun should_include_uppercase_characters_when_enabled() {
-        val generator = PasswordGenerator(hasUpperCase = true)
+        val data = PasswordDataGeneration(
+            hasLowerCase = false,
+            hasUpperCase = true,
+            hasNumbers = false,
+            hasSpecials = false,
+            passwordLength = 20
+        )
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
+
         Assertions.assertTrue(password.any { it.isUpperCase() })
     }
 
     @Test
     fun should_include_number_characters_when_enabled() {
-        val generator = PasswordGenerator(hasNumbers = true)
+        val data = PasswordDataGeneration(
+            hasLowerCase = false,
+            hasUpperCase = false,
+            hasNumbers = true,
+            hasSpecials = false,
+            passwordLength = 20
+        )
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
         Assertions.assertTrue(password.any { it.isDigit() })
     }
 
     @Test
     fun should_include_symbol_characters_when_enabled() {
-        val generator = PasswordGenerator(hasSpecials = true)
+        val data = PasswordDataGeneration(
+            hasLowerCase = false,
+            hasUpperCase = false,
+            hasNumbers = false,
+            hasSpecials = true,
+            passwordLength = 20
+        )
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
+
         Assertions.assertTrue(password.any { it in PasswordPolicy.SYMBOL_CHARS })
     }
 
     @Test
     fun should_throw_exception_when_no_character_sets_enabled() {
-        val generator = PasswordGenerator(
+        val data = PasswordDataGeneration(
             hasLowerCase = false,
             hasUpperCase = false,
             hasNumbers = false,
-            hasSpecials = false
+            hasSpecials = false,
+            passwordLength = 20
         )
+        val generator = PasswordGenerator(data)
 
         Assertions.assertThrows(IllegalArgumentException::class.java) {
             generator.generatePassword()
@@ -87,14 +128,14 @@ class PasswordGeneratorTest {
 
     @Test
     fun should_generate_password_with_all_character_types_when_all_enabled() {
-        val generator = PasswordGenerator(
+        val data = PasswordDataGeneration(
             hasLowerCase = true,
             hasUpperCase = true,
             hasNumbers = true,
             hasSpecials = true,
             passwordLength = 50
         )
-
+        val generator = PasswordGenerator(data)
         val password = generator.generatePassword()
 
         Assertions.assertTrue(password.any { it.isLowerCase() })
