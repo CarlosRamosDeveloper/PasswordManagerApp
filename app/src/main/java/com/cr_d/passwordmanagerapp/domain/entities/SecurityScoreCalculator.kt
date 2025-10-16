5c86206939de98523c32144ee268cf212c70a6d8
Carlos Ramos <carlosramosdev@protonmail.com>
2025-10-16 14:26:20 +0200

    Added generation algorithm and tests, needs to check

--- app/src/main/java/com/cr_d/passwordmanagerapp/domain/entities/SecurityScoreCalculator.kt
+++ app/src/main/java/com/cr_d/passwordmanagerapp/domain/entities/SecurityScoreCalculator.kt
@@ -1,13 +1,39 @@
 package com.cr_d.passwordmanagerapp.domain.entities
 
 import com.cr_d.passwordmanagerapp.domain.value_objects.PasswordDataAnalysis
+import kotlin.math.min
 
 class SecurityScoreCalculator(
-    val password: PasswordDataAnalysis
+    val passwordInfo: PasswordDataAnalysis
 ) {
     fun calculate(): Double {
         var score = 0.0
+        val length = passwordInfo.password.length
 
-        return score
+        score += min(length*4, 40)
+
+        val typeCount = listOf(
+            passwordInfo.hasLowerCase,
+            passwordInfo.hasUpperCase,
+            passwordInfo.hasNumbers,
+            passwordInfo.hasSpecials
+        ).count()
+
+        score += when (typeCount) {
+            2 -> 1.0
+            3 -> 2.0
+            4 -> 3.0
+            else -> 0.0
+        }
+
+        if (length < 8) score -= 5.0
+        else if (length < 12) score -= 2.5
+
+        if (Regex("(.)\\1{2,}").containsMatchIn(passwordInfo.password)) score -= 1.0
+        if (Regex("123|abc|qwe|password|admin", RegexOption.IGNORE_CASE).containsMatchIn(passwordInfo.password)) score -= 3.0
+
+        val final = score.coerceIn(0.0, 10.0)
+
+        return final
     }
 }
