76c362c0aff198830e44f3128d34ea199535b295
Carlos Ramos <carlosramosdev@protonmail.com>
2025-10-17 12:17:26 +0200

    Added IPasswordRepository

---
+++ app/src/main/java/com/cr_d/passwordmanagerapp/application/interfaces/IPasswordRepository.kt
@@ -0,0 +1,14 @@
+package com.cr_d.passwordmanagerapp.application.interfaces
+
+import com.cr_d.passwordmanagerapp.domain.value_objects.PasswordData
+
+interface IPasswordRepository {
+    fun findAll(): List<PasswordData>
+    fun findByApplication(app: String): List<PasswordData>
+    fun findByAccount(account: String): List<PasswordData>
+    fun getPasswordDetails(id: Int): PasswordData?
+    fun save(passwordData: PasswordData)
+    fun update(passwordData: PasswordData)
+    fun delete(id: Int)
+
+}
