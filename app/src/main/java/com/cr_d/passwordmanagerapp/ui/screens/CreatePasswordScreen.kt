f69dedee7388586c09698479695f456ec3e409e6
Carlos Ramos <carlosramosdev@protonmail.com>
2025-11-24 20:44:57 +0100

    Generation Password button working

--- app/src/main/java/com/cr_d/passwordmanagerapp/ui/screens/CreatePasswordScreen.kt
+++ app/src/main/java/com/cr_d/passwordmanagerapp/ui/screens/CreatePasswordScreen.kt
@@ -1,18 +1,16 @@
 package com.cr_d.passwordmanagerapp.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,9 +18,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
+
+import com.cr_d.passwordmanagerapp.domain.entities.PasswordGenerator
+import com.cr_d.passwordmanagerapp.domain.value_objects.PasswordDataGeneration
 
 @Composable
 fun CreatePasswordScreen(innerPadding: PaddingValues){
@@ -40,28 +39,11 @@ fun CreatePasswordScreen(innerPadding: PaddingValues){
         CustomCheckbox("Mayúsculas", hasUpperCase, { hasUpperCase=it })
         CustomCheckbox("Números", hasNumbers, { hasNumbers=it })
         CustomCheckbox("Carácteres especiales", hasSpecials, { hasSpecials=it })
-        //CampoDecimal(passwordLength) { passwordLength=it }
+        CampoDecimal(passwordLength) { passwordLength=it }
+
+        CreatePasswordButton(hasLowerCase, hasUpperCase, hasNumbers, hasSpecials, passwordLength)
     }
 }
-/*
-@Composable
-fun CampoDecimal(value: Int, onValueChange: (Int) -> Unit) {
-    OutlinedTextField(
-        value = value,
-        onValueChange = { newValue ->
-            // Permite solo números y un punto
-            if (newText.matches(Regex("^\\d*\$"))) {
-                val intValue = newText.toIntOrNull() ?: 0
-                onValueChange(intValue)
-            }
-        },
-        label = { Text("Valor decimal") },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Number
-        )
-    )
-}
- */
 
 @Composable
 fun CustomCheckbox(labelText: String, value: Boolean, onValueChange: (Boolean) -> Unit ) {
@@ -75,16 +57,14 @@ fun CustomCheckbox(labelText: String, value: Boolean, onValueChange: (Boolean) -
         Text(labelText)
     }
 }
-/*
+
 @Composable
 fun CampoDecimal(value: Int, onValueChange: (Int) -> Unit) {
-    // Convertimos el Int a String para mostrarlo en el TextField
     val textValue = value.toString()
 
     OutlinedTextField(
         value = textValue,
         onValueChange = { newText ->
-            // Acepta solo números (sin punto si quieres Int)
             if (newText.matches(Regex("^\\d*\$"))) {
                 val intValue = newText.toIntOrNull() ?: 0
                 onValueChange(intValue)
@@ -96,4 +76,25 @@ fun CampoDecimal(value: Int, onValueChange: (Int) -> Unit) {
         )
     )
 }
-*/
+
+@Composable
+fun CreatePasswordButton(
+    hasLowerCase: Boolean,
+    hasUpperCase: Boolean,
+    hasNumbers: Boolean,
+    hasSpecials: Boolean,
+    passwordLength: Int
+) {
+    val passwordDataGeneration = PasswordDataGeneration(hasLowerCase, hasUpperCase, hasNumbers, hasSpecials, passwordLength)
+    val generator = PasswordGenerator(passwordDataGeneration)
+
+    Column {
+        Button(onClick = {
+            val password = generator.generatePassword()
+            Log.d("Password", password)
+        }) {
+            Text("Generar contraseña")
+        }
+    }
+
+}
