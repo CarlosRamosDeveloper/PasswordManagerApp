7b9e07614e34ef242f344211ed63962b4fe078ba
Carlos Ramos <carlosramosdev@protonmail.com>
2025-12-25 12:11:01 +0100

    Basic mode card update

--- app/src/main/java/com/cr_d/passwordmanagerapp/ui/screens/password_detail/components/BasicMode.kt
+++ app/src/main/java/com/cr_d/passwordmanagerapp/ui/screens/password_detail/components/BasicMode.kt
@@ -2,14 +2,18 @@ package com.cr_d.passwordmanagerapp.ui.screens.password_detail.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 import com.cr_d.passwordmanagerapp.ui.models.formatAs
 import com.cr_d.passwordmanagerapp.ui.screens.password_detail.PasswordDetailViewModel
+import com.cr_d.passwordmanagerapp.ui.screens.password_detail.horizontalFramePadding
 import com.cr_d.passwordmanagerapp.ui.screens.settings.SettingsViewModel
 
 @Composable
@@ -20,10 +24,22 @@ fun BasicMode(
     val password = viewModel.uiState.collectAsStateWithLifecycle().value.password ?: return
     val settings = settings.settings.collectAsState().value
 
-    Column (modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        ApplicationTitle(password.appInfo.applicationName)
-        CustomRow("Nombre de usuario", password.appInfo.account)
-        CustomRow("Sitio Web", password.appInfo.url)
-        CustomRow("Última Modificación", password.metadata.lastUpdate.formatAs(settings.dateFormat))
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = horizontalFramePadding, vertical = 10.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ApplicationTitle(password.appInfo.applicationName)
+            CustomRow("Nombre de usuario", password.appInfo.account)
+            CustomRow("Sitio Web", password.appInfo.url)
+            CustomRow(
+                "Última Modificación",
+                password.metadata.lastUpdate.formatAs(settings.dateFormat)
+            )
+        }
     }
 }
