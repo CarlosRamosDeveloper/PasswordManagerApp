8d0946ff6c9495c9afb220f2c3591bcaa4c758fe
Carlos Ramos <carlosramosdev@protonmail.com>
2025-12-26 17:27:35 +0100

    Implemented room in project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("jacoco")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -84,6 +85,11 @@ tasks.register<JacocoReport>("jacocoTestReport") {
 }
 
 dependencies {
+    val roomVersion = "2.8.4"
+
+    implementation("androidx.room:room-runtime:${roomVersion}")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/cr_d/passwordmanagerapp/data/PasswordEntity.kt
+++ app/src/main/java/com/cr_d/passwordmanagerapp/data/PasswordEntity.kt
@@ -1,14 +1,26 @@
 package com.cr_d.passwordmanagerapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDate
 
+@Entity
 data class PasswordEntity(
+    @PrimaryKey
     val id: Int,
+    @ColumnInfo(name = "password")
     val plainPassword: String,
+    @ColumnInfo(name = "app_name")
     val appName: String,
+    @ColumnInfo(name = "app_url")
     val appUrl: String,
+    @ColumnInfo(name = "account")
     val account: String,
+    @ColumnInfo(name = "creation_date")
     val creationDate: LocalDate,
+    @ColumnInfo(name = "last_update")
     val lastUpdate: LocalDate,
+    @ColumnInfo(name = "notes")
     val notes: String,
 )
