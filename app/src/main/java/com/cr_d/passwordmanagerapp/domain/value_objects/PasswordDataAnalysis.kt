99f47a19437ff3d1f4d03693f9a9fca4589d4b38
Carlos Ramos <carlosramosdev@protonmail.com>
2025-12-26 15:51:53 +0100

    Removed PasswordDataAnalysis

--- app/src/main/java/com/cr_d/passwordmanagerapp/domain/value_objects/PasswordDataAnalysis.kt
+++
@@ -1,8 +0,0 @@
-package com.cr_d.passwordmanagerapp.domain.value_objects
-
-data class PasswordDataAnalysis(
-    val hasLowerCase : Boolean,
-    val hasUpperCase : Boolean,
-    val hasNumbers : Boolean,
-    val hasSpecials : Boolean,
-)
