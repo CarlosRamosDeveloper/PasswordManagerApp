08c8e2024df504447c09148af998273c7e4ab515
Carlos Ramos <carlosramosdev@protonmail.com>
2025-12-26 15:52:33 +0100

    Cleaned import

--- app/src/main/java/com/cr_d/passwordmanagerapp/domain/value_objects/PasswordMetadata.kt
+++ app/src/main/java/com/cr_d/passwordmanagerapp/domain/value_objects/PasswordMetadata.kt
@@ -1,7 +1,5 @@
 package com.cr_d.passwordmanagerapp.domain.value_objects
 
-import java.time.LocalDate
-
 data class PasswordMetadata(
     val hasLowerCase: Boolean,
     val hasUpperCase: Boolean,
