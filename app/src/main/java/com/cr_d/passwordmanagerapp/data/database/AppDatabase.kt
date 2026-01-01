83a7c792e1b329e7f10c0525371dc898a1c33dca
Carlos Ramos <carlosramosdev@protonmail.com>
2026-01-01 16:18:06 +0100

    Updated version at database

--- app/src/main/java/com/cr_d/passwordmanagerapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/cr_d/passwordmanagerapp/data/database/AppDatabase.kt
@@ -8,7 +8,7 @@ import com.cr_d.passwordmanagerapp.data.daos.PasswordDao
 import com.cr_d.passwordmanagerapp.data.entities.AccountEntity
 import com.cr_d.passwordmanagerapp.data.entities.PasswordEntity
 
-@Database(entities = [PasswordEntity::class, AccountEntity::class], version = 1, exportSchema = false)
+@Database(entities = [PasswordEntity::class, AccountEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun passwordDao() : PasswordDao
     abstract fun accountDao() : AccountDao
